e3a36f3160fc473dd5a46c55b41836f8c4e0cf48
Antony <A.Krutenok@gmail.com>
2020-01-14 14:53:21 +0300

    feat: react-router, bootstrap navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,44 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
-import Nav from '../Nav';
 import List from '../Screens/List';
 import Calendar from '../Screens/Calendar';
 import Focus from '../Screens/Focus';
 import Notes from '../Screens/Notes';
 import Task from '../Screens/Task';
 import Projects from '../Screens/Projects/Projects';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { navIsActive: false,
-    activeScreen: 'list' };
+    this.state = { navIsActive: false, activeScreen: 'list' };
   }
 
   setNav() {
-    this.setState({ navIsActive: !this.state.navIsActive})
+    this.setState({ navIsActive: !this.state.navIsActive });
   }
 
   setScreen(target) {
     this.setNav();
-    this.setState({activeScreen: target})
+    this.setState({ activeScreen: target });
   }
-  
+
   render() {
     return (
-      <div className="App">
-        <Header navActive={this.state.navIsActive} setNav={this.setNav.bind(this)} curScreen={this.state.activeScreen} />
-        {this.state.navIsActive && <Nav setScreen={this.setScreen.bind(this)} />}
-        {this.state.activeScreen === 'list' && <List />}
-        {this.state.activeScreen === 'focus' && <Focus />}
-        {this.state.activeScreen === 'calendar' && <Calendar />}
-        {this.state.activeScreen === 'notes' && <Notes />}
+      <div className='App'>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path='/todo' component={List} />
+            <Route path='/calendar' component={Calendar} />
+            <Route path='/focus' component={Focus} />
+            <Route path='/notes' component={Notes} />
+            <Route path='/projects' component={Projects} />
+          </Switch>
+        </Router>
         {this.state.activeScreen === 'task' && <Task />}
-        {this.state.activeScreen === 'projects' && <Projects />}
         <Footer />
       </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
-const Header = (props) => {
-  const btnTitle = `${props.navActive ? 'Close' : 'Show'} nav`;
+const Header = props => {
+  function navItem(path, title) {
+    return (
+      <li
+        className='nav-item'
+        data-toggle='collapse'
+        data-target='#navbarToggler'
+      >
+        <NavLink className='nav-link' to={`/${path}`}>
+          {title}
+        </NavLink>
+      </li>
+    );
+  }
+
   return (
-    <section className="header">
-      <button onClick={props.setNav}>{ btnTitle }</button>
-      <span>TO-DO COMBAINE</span>
-      <span>{props.curScreen}</span>
+    <section className='header'>
+      <nav className='navbar navbar-light bg-light'>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarToggler'
+          aria-controls='navbarToggler'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarToggler'>
+          <div>
+            <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+            {navItem('todo', 'Список дел')}
+            {navItem('calendar', 'Календарь')}
+            {navItem('focus', 'Фокусировка')}
+            {navItem('notes', 'Заметки')}
+            {navItem('projects', 'Проекты')}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </section>
   );
-}
+};
 
 export default Header;
